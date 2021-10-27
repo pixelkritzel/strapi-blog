54d1ff2e8f808c0f5e1fe510a4d45d442f9f0ad3
pixelkritzel <timo@pixelkritzel.de>
2021-10-28 00:08:38 +0200

    ---

--- api/post/models/post.js
+++ api/post/models/post.js
@@ -1,8 +1,22 @@
-'use strict';
+"use strict";
+
+const marked = require("marked");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      console.log(data);
+      data.html_content = marked(data.content);
+    },
+    beforeUpdate: async (params, data) => {
+      if (data.content) {
+        data.html_content = marked(data.content);
+      }
+    },
+  },
+};
