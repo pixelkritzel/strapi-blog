7d97e1ab6b47eb5bcac072ce34a666abce0e1523
pixelkritzel <timo@pixelkritzel.de>
2021-10-28 00:09:27 +0200

    ---

---
+++ api/page/controllers/page.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.page.findOne({ slug: id });
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
+};
